f6c2cc70e0b021b2980799ab8300b27769b763e4
NathiSibiya <nkosi.n.sibiya@outlook.com>
2023-02-16 13:08:16 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - programme run time always starts here
+ *
+ * Return: if no errors return zero
+ *
+ */
+int main(void)
+
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+
+}
