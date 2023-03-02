f4f02563939979aec4ab717523d5b01405867ed1
NathiSibiya <nkosi.n.sibiya@outlook.com>
2023-03-01 23:14:28 -0800

    cap string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,11 +11,12 @@ char *cap_string(char *str)
 {
 	int index = 0;
 
+	while (str[index])
+	{
+
 	while (!(str[index] >= 'a' && str[index] <= 'z'))
-		index++
-		{
-			if (str[index - 1]
-				str[index - 1] == ' ' ||
+		index++;
+			if (str[index - 1] == ' ' ||
 				str[index - 1] == '\t' ||
 				str[index - 1] == '\n' ||
 				str[index - 1] == ',' ||
@@ -33,7 +34,7 @@ char *cap_string(char *str)
 				str[index] -= 32;
 
 	index++;
-}
-	return (str);
 	}
 
+	return (str);
+}
