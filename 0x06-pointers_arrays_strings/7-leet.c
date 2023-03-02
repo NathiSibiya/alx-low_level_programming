d04d2704a10f648104ca7453ba8ea71febc1a3e4
NathiSibiya <nkosi.n.sibiya@outlook.com>
2023-03-01 21:51:22 -0800

    renamed directory

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string to 1337
+ * @str: thr string to be encoded
+ *
+ * Return: a pointer to encoded string
+ */
+
+char *leet(char *)
+{
+	int indx1 = 0, indx2;
+	char leet[8] = {'0', 'L', 'E', 'A', '?', 'T'};
+
+	while (str[indx1])
+	{
+		for (indx2 = 0; indx2 <= 7; indx++)
+		{
+			if (str[indx1] == leet[indx2] ||
+					str[indx] - 32 == leet[indx2])
+				str[indx1] = indx2 + '0';
+		}
+		indx++;
+	}
+
+	return (str);
+}
