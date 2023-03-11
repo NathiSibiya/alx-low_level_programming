1619595c91d15c788c0ea4e4cd2faea08a119d92
NathiSibiya <nkosi.nsibiya@outlook.com>
2023-03-11 19:18:25 +0200

    100

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - prints the min number and make change
+* @argc: count
+* @argv: vector
+*
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int cents, coins = 0;
+
+	if (argc != 2)
+	{
+		printf("Error");
+		return (1);
+	}
+
+	cents = atoi(argv[1]);
+
+	while (cents > 0)
+	{
+		coins++;
+		if ((cents - 25) >= 0)
+	{
+		cents -= 25;
+		continue;
+	}
+	if ((cents - 10) >= 0)
+	{
+		cents -= 10;
+		continue;
+	}
+	if ((cents - 5) >= 0)
+	{
+
+		cents -= 5;
+		continue;
+	}
+	if ((cents - 2) >= 0)
+	{
+
+		cents -= 2;
+		continue;
+	}
+	cents--;
+}
+printf("%d\n", coins);
+
+return (0);
+}
