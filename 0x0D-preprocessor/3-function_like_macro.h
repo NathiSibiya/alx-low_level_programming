fa131cffe86760b9991d9646d6338d5a4e44fb1f
NathiSibiya <nkosi.n.sibiya@outlook.com>
2023-03-20 13:40:40 -0700

    Function like macro

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/*
+ * File: 3-function_like_macro.h
+ * Auth: Nathi Sibiya
+ * Desc: defines macro ABS(x) of a number x
+ */
+
+#define ABS(x) ((x) < 0 ? (-x) : (x))
+
+#endif
