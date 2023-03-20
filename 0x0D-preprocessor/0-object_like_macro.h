b2fa90817c6cc745ce18dd992af91d18289f0ab0
NathiSibiya <nkosi.n.sibiya@outlook.com>
2023-03-20 12:57:06 -0700

    Object-like Macro

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,15 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/*
+ * File: 0-object_like_macro.h
+ * Auth: Nathi Sibiya
+ * Desc: header file defines a macro named
+ *	SIZE as an abbreviation for token 1024
+ */
+
+
+#define SIZE 1024
+
+#endif
+
