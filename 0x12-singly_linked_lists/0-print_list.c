7a45ef040b0071a672894070ed3bab8f87a38584
NathiSibiya <nkosi.n.sibiya@outlook.com>
2023-03-30 15:22:23 -0700

    fixed code errors

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,20 +10,19 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
+	size_t s = 0;
 
 	while (h)
 	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
+	if (!h->str)
+		printf("[0] (nil)\n");
+	else
+		printf("[%u] %s\n", h->len, h->str);
 
-		else
-			printf("[%d} %s\n", h->len, h->str);
-
-		nodes++;
-		h = h->next;
-
+	h = h->next;
+	s++;
 	}
 
-	return (nodes);
+	return (s);
 }
+
